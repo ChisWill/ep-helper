e3305e60b521999f1eee4e0c2f564d24e185d5e3
ChisWill <459967016@qq.com>
2021-07-22 20:03:24 +0800

    v0.2.11

--- src/Date.php
+++ src/Date.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Ep\Helper;
 
+use DateTime;
+use DateTimeZone;
+
 class Date
 {
     /**
@@ -13,11 +16,28 @@ class Date
      * 
      * @return string
      */
-    public static function fromUnix(?int $timestamp = null): string
+    public static function fromUnix(int $timestamp = null): string
     {
         if ($timestamp === null) {
             $timestamp = time();
         }
         return date('Y-m-d H:i:s', $timestamp);
+    }
+
+    /**
+     * 获得 GMT 格式时间
+     * 
+     * @param  int|null $timestamp 时间戳，不传默认为当前时间
+     * 
+     * @return string
+     */
+    public static function toGMT(int $timestamp = null): string
+    {
+        if ($timestamp === null) {
+            $timestamp = time();
+        }
+        return DateTime::createFromFormat('U', (string) $timestamp)
+            ->setTimezone(new DateTimeZone('UTC'))
+            ->format('D, d M Y H:i:s') . ' GMT';
     }
 }
